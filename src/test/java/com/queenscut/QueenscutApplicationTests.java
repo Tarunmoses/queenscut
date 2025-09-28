92ad444c3a39b2b22791177a687d6cd3d878275a
tarunmoses <tarunmoses99@gmail.com>
2025-09-29 02:16:46 +0530

    initial commit

---
+++ src/test/java/com/queenscut/QueenscutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queenscut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueenscutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
